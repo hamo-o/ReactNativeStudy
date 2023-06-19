b60915040041d1403cdad74f700362e7ad845fa6
hamo-o <yheel369@gmail.com>
2023-06-19 21:21:07 +0900

    feat #1 위도, 경도 표시

--- Study/Ham/Week3/src/components/maps/Map.tsx
+++ Study/Ham/Week3/src/components/maps/Map.tsx
@@ -1,5 +1,7 @@
-import {View} from 'react-native';
+import {useState} from 'react';
+import {Text, View} from 'react-native';
 import {usePermissions} from '../../hooks/usePermissions';
+import Geolocation from '@react-native-community/geolocation';
 
 const Map = () => {
   usePermissions({
@@ -8,7 +10,21 @@ const Map = () => {
     buttonPositive: '확인',
   });
 
-  return <View></View>;
+  const [latitude, setLatitude] = useState<string | null>(null);
+  const [longitude, setLongitude] = useState<string | null>(null);
+  Geolocation.getCurrentPosition(position => {
+    const lati = JSON.stringify(position.coords.latitude);
+    const long = JSON.stringify(position.coords.longitude);
+    setLatitude(lati);
+    setLongitude(long);
+  });
+
+  return (
+    <View>
+      <Text>{latitude}</Text>
+      <Text>{longitude}</Text>
+    </View>
+  );
 };
 
 export default Map;
